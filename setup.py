4f9814e8d50a2a3acfd4aec1066ddc77086623fb
Adam Hooper <adam@adamhooper.com>
2019-08-07 15:13:52 -0400

    Test with pandas 0.25.0

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from setuptools import setup
 
 setup(
-    name='wordcloud',
-    version='0.0.1',
-    description='Show most-common words in a text column',
-    author='Adam Hooper',
-    author_email='adam@adamhooper.com',
-    url='https://github.com/CJWorkbench/wordcloud',
-    packages=[''],
-    py_modules=['wordcloud'],
-    install_requires=['pandas==0.24.1', 'nltk==3.3.0', 'fb-re2==1.0.7']
+    name="wordcloud",
+    version="0.0.1",
+    description="Show most-common words in a text column",
+    author="Adam Hooper",
+    author_email="adam@adamhooper.com",
+    url="https://github.com/CJWorkbench/wordcloud",
+    packages=[""],
+    py_modules=["wordcloud"],
+    install_requires=["pandas==0.25.0", "nltk==3.3.0", "fb-re2==1.0.7"],
 )
